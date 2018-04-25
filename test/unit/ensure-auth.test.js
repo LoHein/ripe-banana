a5528b4f4620ee254dea0643ac23f8b71a43eb6a
Charlie <heinercj@gmail.com>
2018-04-25 10:41:53 -0700

    ensure auth unit tests

---
+++ test/unit/ensure-auth.test.js
@@ -0,0 +1,39 @@
+const { assert } = require('chai');
+const createEnsureAuth = require('../../lib/util/ensure-auth');
+const tokenService = require('../../lib/util/token-service');
+
+describe.only('ensure auth middleware', () => {
+    const reviewer = { _id: 123 };
+    let token = '';
+    beforeEach(() => token = tokenService.sign(reviewer));
+
+    const ensureAuth = createEnsureAuth();
+
+    it('adds payload as req.user on success', done => {
+        const req = {
+            get(header) {
+                if(header === 'Authorization') return token;
+            }
+        };
+
+        const next = () => {
+            assert.equal(req.reviewer.id, reviewer._id);
+            done();
+        };
+
+        ensureAuth(req, null, next);
+    });
+
+    it('calls next with error when token is bad', done => {
+        const req = {
+            get() { return 'bad-token'; }
+        };
+
+        const next = err => {
+            assert.equal(err.status, 401);
+            done();
+        };
+
+        ensureAuth(req, null, next);
+    });
+});
